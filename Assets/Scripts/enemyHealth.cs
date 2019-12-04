dd334064981df07a127a91febdc674f47ea8fef2
Vytautas Sipavicius <vytasip@hotmail.com>
2019-12-04 17:23:51 +0000

    Started setting enemy health

--- Assets/Scripts/enemyHealth.cs
+++ Assets/Scripts/enemyHealth.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class enemyHealth : MonoBehaviour
 {
-    public HealthBar healthBar;
     public static HealthSystem playerHealth;
     // Start is called before the first frame update
+    
     void Start()
     {
         playerHealth = new HealthSystem(100);
-        healthBar.Setup(playerHealth);
     }
 
     // Update is called once per frame
@@ -22,4 +21,5 @@ public class GameManager : MonoBehaviour
     public static void damagePlayers(int damage){
         playerHealth.Damage(damage);
     }
+
 }
