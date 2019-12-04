34ff0c48b659041d444c8ee5f5ae5daec25b39a2
Vytautas Sipavicius <vytasip@hotmail.com>
2019-12-04 14:31:16 +0000

    Health - System and Bar
    Health System and bar implemented, healthbar and system for players added aswell

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthSystem playerHealth = new HealthSystem(100);
+        healthBar.Setup(playerHealth);
+        playerHealth.Damage(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class HealthSystem
 {
+    public event EventHandler OnHealthChanged; 
     private int health;
+    private int healthMax;
 
     public HealthSystem(int health){
         this.health = health;
+        this.healthMax = health;
     }
 
     public int GetHealth(){
         return health;
     }
 
+    public float GetHealthPercent(){
+        return (float)health/healthMax;
+    }
     public void Damage(int damage_amount){
         health -= damage_amount;
+        if(health < 0) health = 0;
+        if(OnHealthChanged != null) OnHealthChanged(this, EventArgs.Empty);
     }
 }
