5b4daa4aae2e9ca21331adb779266ce5d9b83bfc
Arnar Snaer <arnarsnaer98@gmail.com>
2019-12-05 19:28:42 +0000

    More mess
    pls fix

--- Assets/Scripts/enemyDamage.cs
+++ Assets/Scripts/enemyDamage.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class enemyDamage : MonoBehaviour
 {
+    private SpawnManager _spawnManager;
+
     public int damage_amount;
     void OnCollisionEnter2D(Collision2D enemy){
         if(enemy != null){
             if(enemy.gameObject.GetComponent<TankMovement>() != null){
                 GameManager.damagePlayers(damage_amount);
                 Destroy (this.gameObject);
+                _spawnManager.EnemyDefeated();
+                Debug.Log("Enemy defeated!");
             }
         }
     }
