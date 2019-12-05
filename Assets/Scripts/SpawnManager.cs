314b4af7574d2539a1ee6789a068654330780166
Bjarni <bjarnib18@ru.is>
2019-12-05 20:58:01 +0000

    Created a Win Scene

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class Wave
@@ -40,7 +41,7 @@ public class SpawnManager : MonoBehaviour
         // win
         if (_currentWave > _totalWaves)
         {
-            return;
+            SceneManager.LoadScene("Win");
         }
 
         _totalEnemiesInCurrentWave = Waves[_currentWave].EnemiesPerWave;
