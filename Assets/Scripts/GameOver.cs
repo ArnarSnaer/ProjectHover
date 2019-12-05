105518e2cfb7b8204414c83cc69142f0404846ae
Bjarni <bjarnib18@ru.is>
2019-12-05 20:49:23 +0000

    Created Scene Transitions
    From MainMenu to MainGame, to GameOver

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("P1 PS4 X") || Input.GetButton("P2 PS4 Circle"))
+        {
+            Debug.Log("LOAD");
+            SceneManager.LoadScene("MainMenu");        
+        }
+    }
+}
