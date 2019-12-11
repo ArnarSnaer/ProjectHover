abbef0f1e93e537f9298207e808917364ec988e9
Arnar Snaer <arnarsnaer98@gmail.com>
2019-12-11 16:01:13 +0000

    Explosion + sound on kill
    Yay on feedback!

--- Assets/Scripts/enemyHealth.cs
+++ Assets/Scripts/enemyHealth.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class enemyHealth : MonoBehaviour
 {
     private SpawnManager _spawnManager;
-    public AudioSource enemyKill;
+    public AudioClip enemyKill;
+    AudioSource audioSource;
     public int max_health;
     // Start is called before the first frame update
     public HealthSystem health;
     private Material matWhite;
     private Material matDefault;
-    private UnityEngine.Object explosionRef;
+    public GameObject explosionRef;
     SpriteRenderer sr;
     void Start()
     {   
@@ -19,11 +20,7 @@ public class enemyHealth : MonoBehaviour
         health = new HealthSystem(max_health);
         health.Ded += HealthSystem_Ded;
 
-        sr = GetComponent<SpriteRenderer>();
-        matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
-        matDefault = sr.material;
-
-        explosionRef = Resources.Load("Explosion");
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -34,13 +31,12 @@ public class enemyHealth : MonoBehaviour
 
     private void HealthSystem_Ded(object sender, System.EventArgs e){
         Death();
-        enemyKill.Play();
-        //GameObject explosion = (GameObject)Instantiate(explosionRef);
-        //explosion.transform.position = new Vector3(transform.position.x, transform.position.y + .3f, transform.position.z);
         Destroy(this.gameObject);
     }
     private void Death(){
         _spawnManager.EnemyDefeated();
+        Instantiate(explosionRef, this.transform.position, Quaternion.identity);
+        audioSource.PlayOneShot(enemyKill, 1F);
         Debug.Log("Enemy defeated!");
     }
 }
